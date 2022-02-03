022a78398c62bca1afd8d37b8102caace32209e7
sajith <sajithd784@gmail.com>
2022-02-03 17:00:04 +0530

    finalized splashScreen form and import DB form

---
+++ src/assets/db-script.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS student_attendance;
+
+CREATE DATABASE student_attendance;
+
+USE student_attendance;
+
+CREATE TABLE student
+(
+    id VARCHAR(30) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    picture LONGBLOB NOT NULL
+);
+
+CREATE TABLE user
+(
+    username VARCHAR(30) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    role ENUM ('ADMIN', 'USER') NOT NULL
+);
+
+CREATE TABLE attendance
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME NOT NULL,
+    status ENUM ('IN', 'OUT') NOT NULL,
+    student_id VARCHAR(30) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    CONSTRAINT fk_attendance_student FOREIGN KEY (student_id) REFERENCES student(id),
+    CONSTRAINT fk_attendance_user FOREIGN KEY (username) REFERENCES user(username)
+);

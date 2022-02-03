dca5186cd2f2035c7e446e7bb520ba33871163d1
sajith <sajithd784@gmail.com>
2022-02-03 11:17:30 +0530

    add Import db form

---
+++ src/controller/ImportDBFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class ImportDBFormController {
+    public RadioButton rdoRestore;
+    public TextField txtBrowse;
+    public Button btnBrowse;
+    public RadioButton rdoFirstTime;
+    public Button btnOK;
+
+    public void btnBrowse_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOK_OnAction(ActionEvent actionEvent) {
+    }
+}
